f494fceb78e4d5607d21031faea77c1cc44bfe16
priyariya04 <59275582+priyariya04@users.noreply.github.com>
2020-01-03 10:51:29 +0530

    Inheritance

---
+++ Inheritance/src/Dinosur/LavaDino.java
@@ -0,0 +1,31 @@
+package Dinosur;
+
+public class LavaDino extends Dinosaur {
+private boolean hasHeatResistance;
+	
+	public LavaDino(String species, String locomotionType, boolean hasHeatResistance) {
+		super(species, locomotionType);
+		this.hasHeatResistance = hasHeatResistance;
+}
+	public void  display(String species,String locomotionType,boolean hasHeatResistance)
+	{
+		
+			//boolean a=true;
+		super.display(species,locomotionType,hasHeatResistance);
+
+		if(hasHeatResistance)
+		{
+			
+			System.out.println("HasHeatResistance\t"+this.hasHeatResistance);
+			System.out.println("Species "+species+"exist");
+		}
+		else
+		{
+			
+			System.out.println("HasHeatResistance\t"+this.hasHeatResistance);
+			System.out.println("Species "+species+" not exist");
+		}
+		
+	}
+
+}
