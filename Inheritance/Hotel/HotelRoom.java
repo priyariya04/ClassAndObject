7ed0aaf369ae79ba4967963d1cd1a8427363110a
priyariya04 <59275582+priyariya04@users.noreply.github.com>
2020-01-06 17:42:39 +0530

    Inheritance

---
+++ Inheritance/Hotel/HotelRoom.java
@@ -0,0 +1,19 @@
+package Hotel;
+
+public class HotelRoom {
+	protected String name;
+
+
+	public HotelRoom(String name) {
+
+	this.name = name;
+	}
+
+	public String getName() {
+	return name;
+	}
+
+	public void setName(String name) {
+	this.name = name;
+	}
+}
